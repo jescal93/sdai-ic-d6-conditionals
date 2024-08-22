7e3f60e488a4c14f47a49b73fa0fc141a83c1593
jescal93 <jasonescalera93@gmail.com>
2024-08-22 04:20:39 +0000

    js excersice

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // all code for this lab to be completed in this file
+let currentTemp = 40
+
+if (currentTemp > 32) {
+    console.log("it is not freezing.")
+} else {
+    console.log("it is freezing")
+}
+
+const myAge = 30
+const yourAge = 36
+
+if (myAge === yourAge) {
+    console.log("we are the same age.")
+} else if (myAge > yourAge) {
+    console.log("I am older than you.")
+} else {
+    console.log("I am younger than You")
+}
+
+let isLoggedIn = true
+
+if (isLoggedIn = true) {
+    console.log("Welcome you are logged in.")
+} else if (isLoggedIn = false) {
+    console.log("You are not logged in yet.")
+}
